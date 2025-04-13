eef2e6356d6606b50b873f691fe09fcab38bc8f1
Sanju <gamingsanju15062001@gmail.com>
2025-04-13 13:22:27 +0530

    Add project files.

---
+++ TaskManagemnetAPI/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagemnetAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
